f4df6509e7f0c77b8cbf316c3c0471a80c191816
Se-Joon Chung <i@se-joon.com>
2013-10-12 00:07:54 -0500

    Adding a Python dummy client

---
+++ dummy-t25/client.py
@@ -0,0 +1,164 @@
+#! /usr/bin/env python2
+
+# Basic Test Client for MechMania 19
+# Copyright (c) 2013 Association for Computing Machinery at the University
+# of Illinois, Urbana-Champaign. Inherits license from main MechMania 19 code.
+
+import json
+import logging
+import random
+import socket
+
+from ship import Ship
+
+# TODO (competitors): This is arbitrary but should be large enough
+MAX_BUFFER = 65565
+
+class Client(object):
+    """
+    A class for managing the client's connection.
+
+    TODO (competitors): You should add the API functions you need here. You can
+    remove the inheritance from object if "new style" classes freak you out, it
+    isn't important.
+    """
+
+    def require_connection(func):
+        """
+        This is a decorator to wrap a function to require a server connection.
+
+        func -- A Client class function with self as the first argument.
+        """
+        def wrapped(self, *args):
+            if self.sock == None:
+                logging.error("Connection not established")
+            else:
+                return func(self, *args)
+
+        return wrapped
+
+    def __init__(self, host, port, name):
+        """
+        Initialize a client for interacting with the game.
+
+        host -- The hostname of the server to connect
+                (e.g.  "example.com")
+        port -- The port to connect on (e.g. 6969)
+        name -- Unique name of the client connecting
+        """
+        self.host = host
+        self.port = port
+        self.name = name
+        self.sock = None
+        self.token = ""
+        self.resources = 0
+
+    def connect(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+        logging.info("Connection target is %s:%d", self.host, self.port)
+        self.sock.connect((self.host, self.port))
+        logging.info("Connection established")
+
+    @require_connection
+    def prep_game(self, shiparray):
+        """
+        Handle all the pre-work for game setup.
+
+        This function won't return until the server is connected and the game
+        begins. There's no real need to call it asynchronously, as you can't
+        actually do anything until the server is connected.
+
+        shiparray -- The initial positions for all ships
+        """
+
+        # Step 1: Set up the initial data payload
+        payload = {'playerName': self.name}
+        # TODO (competitors): This is really ugly because the main ship is
+        # special cased. I'm sorry. Feel free to fix.
+        payload['mainShip'] = shiparray[0].getJSON()
+        payload['ships'] = [ship.getJSON() for ship in shiparray[1:]]
+
+        # Step 2: Transmit the payload and receive the reply
+        logging.info("Transmitting start package to server...")
+        reply = self._send_payload(payload)
+
+        # Step 3: Process the reply
+        self._process_reply(reply, setup=True)
+
+    def attack_forever(self):
+        """
+        This method does nothing remotely reasonable.
+
+        If you want a picture of the future, imagine a boot stomping on a
+        nautical battleground--forever.
+        """
+        self.alive = True
+        while(self.alive):
+            # Step 1: Construct a turn payload
+            payload = {'playerToken': self.token}
+            payload['shipActions'] = []
+
+            # Step 2: Transmit turn payload and wait for the reply
+            reply = self._send_payload(payload)
+
+            # Step 3: Process the reply
+            self._process_reply(reply)
+
+    def _send_payload(self, payload):
+        """
+        Send a payload to the server and return the deserialized reply.
+
+        payload -- Payload dictionary to send out.
+
+        Returns a dictionary with the reply data.
+        """
+        # Send this information to the server
+        self.sock.sendall(json.dumps(payload) + '\n')
+        return json.loads(self.sock.recv(MAX_BUFFER))
+
+    def _process_reply(self, reply, setup=False):
+        """
+        Process a reply from the server.
+
+        This method could use a lot more intelligence. As is, it ignores almost
+        everything in the reply payload.
+
+        reply -- The reply dictionary to process
+        setup (default=False) -- Whether this is a new server connection
+        """
+        if reply['error']:
+            logging.warn("Dummy: Problem with last transmit: %s", reply['error'])
+            return
+        if setup:
+            self.token = reply['playerToken']
+        self.resources = reply['resources']
+
+def main():
+    establish_logger(logging.DEBUG)
+    ships = generate_ships()
+
+    # TODO (competitors): Change the client name, update ship positions, etc.
+    client = Client("localhost", 6969, "dummy")
+    client.connect()
+    client.prep_game(ships)
+    # TODO (competitors): make your game do something now!
+    client.attack_forever()
+
+
+def establish_logger(loglevel):
+    logging.basicConfig(format="%(asctime)s %(message)s",
+            datefmt='%m/%d/%Y %I:%M:%S %p', level=loglevel)
+
+def generate_ships():
+    """This generates ships non-strategically for testing purposes."""
+    # Let's get some ships
+    ships = []
+    ships.append(Ship("M", 5, 5, "H"))
+    ships.append(Ship("D", 5, 6, "H"))
+    ships.append(Ship("D", 5, 7, "H"))
+    ships.append(Ship("D" ,5, 8, "H"))
+    return ships
+
+if __name__ == "__main__":
+    main()
---
+++ dummy-t25/ship.py
@@ -0,0 +1,43 @@
+# Ship class for Python Test Client, MM19
+
+# Copyright (c) 2013 Association for Computing Machinery at the University
+# of Illinois, Urbana-Champaign. Inherits license from main MechMania 19 code.
+
+import random
+
+class Ship(object):
+    """
+    Really bare bones ship class.
+
+    You probably want to flesh this out a bit (for instance, use some constants
+    instead of all the hard-coded things) if you're using the client for
+    development.
+
+    It's not well documented because everything is either obvious or kind of
+    stupid. I'm sorry.
+    """
+
+    def __init__(self, ship_type, x, y, orient):
+      self.ship_type = ship_type
+      self.x = x
+      self.y = y
+      self.orient = orient
+
+    def getJSON(self):
+      """Return the JSON dictionary representation of the ship."""
+      out = {'xCoord': self.x, 'yCoord': self.y, 'orientation': self.orient}
+      if self.ship_type is not "M":
+        out['type'] = self.ship_type
+      return out
+
+    @classmethod
+    def random_ship(Ship, ship_type):
+      """
+      Static method for placing a ship randomly.
+
+      ship_type: The type of ship.
+      """
+      x = random.randint(0, 99)
+      y = random.randint(0, 99)
+      orient = random.choice(["H", "V"])
+      return Ship(ship_type, x, y, orient)
